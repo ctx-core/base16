ceff35a6fae88337f7f60f91533f0751781b9349
Brian Takita <brian.takita@gmail.com>
2021-05-24 19:54:42 -0400

    ## 5.2.1
    ### Patch Changes
    
    - .gitignore: - \*.js

---
+++ src/base16_random.js
@@ -0,0 +1,10 @@
+export function base16_random(length = 5) {
+    const value_a1 = [];
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const length__possible = possible.length;
+    for (let i = 0; i < length; i++) {
+        value_a1.push(possible.charAt(Math.floor(Math.random() * length__possible)));
+    }
+    return value_a1.join('');
+}
+export { base16_random as random__base16 };
